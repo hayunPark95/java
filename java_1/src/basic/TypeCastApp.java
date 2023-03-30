e1e32463c0b02c8b896b25caf9a5a264e6a9db80
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-03-30 14:33:48 +0900

    3/30

---
+++ java_1/src/basic/TypeCastApp.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class TypeCastApp {
+	public static void main(String[] args) {
+		System.out.println("결과 = "+(10+13.5));
+		
+		double su=10;
+		System.out.println("su = "+su);
+		System.out.println("결과 = "+(35/10));
+	}
+}
