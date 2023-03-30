f34ce66b259db560dfcf152e6b8b0e80e815008c
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-03-30 11:49:41 +0900

    3/30

--- java_1/src/basic/FirstEclipseApp.java
+++ java_1/src/basic/FirstEclipseApp.java
@@ -4,7 +4,7 @@ public class FirstEclipseApp {
 	public static void main(String[] args) {
 		
 		System.out.print("안녕하세요.");//출력 메소드 호출
-		System.out.println("이클립스를 이용한 첫번째 프로그램입니다.");/* 출력 메소드 호출 */
+		System.out.println("이클립스를 이용한 첫번째 프로그램입니다.");
 		System.out.println("재미있게 프로그램을 만들어 봐요.");
 	}
 }
