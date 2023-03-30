dff554a72c8ad76251c930fb2788910ca37d0431
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-03-30 11:58:23 +0900

    3/30

--- java_1/src/basic/WhyError.java
+++ java_1/src/basic/WhyError.java
@@ -2,6 +2,6 @@ package basic;
 
 public class WhyError {
 	public static void main(String[] args) {
-		System.out.println("왜 깃허브에 자꾸 푸시가 안되냐 이클립스야");
+		System.out.println("왜 깃허브에 자꾸 푸시가 안되니 이클립스야");
 	}
 }
