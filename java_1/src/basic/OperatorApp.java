2cdf4c8997e4a596ba4d7f14fea760e603f4bfa6
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-03-30 16:54:22 +0900

    3/30 3일차

--- java_1/src/basic/OperatorApp.java
+++ java_1/src/basic/OperatorApp.java
@@ -11,5 +11,34 @@ public class OperatorApp {
 		
 		System.out.println("20>10 = "+(20>10));
 		System.out.println("!20>10 = "+!(20>10));
+		
+		int a2=10, a3=10;
+		System.out.println("a2 = "+a2+",a3 = "+a3);
+		
+		++a2;
+		--a3;
+		System.out.println("a2 = "+a2+",a3 = "+a3);
+		
+		int a4=10, a5=10;
+		System.out.println("a4 = "+a4+",a5 = "+a5);
+		
+		int result1=++a4;
+		int result2=a5++;
+		System.out.println("a4 = "+a4+",a5 = "+a5);
+		System.out.println("result1 = "+result1+",result2 = "+result2);
+		
+		int b1=20,b2=10;
+		System.out.println(b1+" * "+b2+" = "+(b1*b2));
+		System.out.println(b1+" / "+b2+" = "+(b1/b2));
+		System.out.println(b1+" % "+b2+" = "+(b1%b2));
+		System.out.println(b1+" + "+b2+" = "+(b1+b2));
+		System.out.println(b1+" - "+b2+" = "+(b1-b2));
+		System.out.println("=====================================================");
+		
+		System.out.println("20 > 10 ="+(20>10));
+		System.out.println("20 < 10 ="+(20<10));
+		System.out.println("20 == 10 ="+(20==10));
+		System.out.println("20 != 10 ="+(20!=10));
+		
 	}
 }
