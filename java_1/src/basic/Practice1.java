1e809b456b37432603d9ee80a9e5837361605b86
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-03-30 09:36:13 +0900

    3/30

---
+++ java_1/src/basic/Practice1.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		boolean a=true;
+		boolean b=false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		System.out.println(3<4);
+		System.out.println(3==4);
+		System.out.println(3<2);
+		System.out.println(3<-3);
+		
+	}
+}
