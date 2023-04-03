32f324dcd478dcfec770103c03b5381def2d428c
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-03 18:11:23 +0900

    4/3 5일차 5일차 과제 및 이전 과제들 최신화함

--- java_1/src/example/OperatorExample.java
+++ java_1/src/example/OperatorExample.java
@@ -3,34 +3,23 @@ package example;
 public class OperatorExample {
 	public static void main(String[] args) {
 		//245678초를 일시분초 형식으로 변환하여 출력하세요.
-		int totSec=245678;
+		int cho=245678;
+		System.out.print(cho+"초 = ");
 		
-		int day=totSec/(60*60*24);
-		int hour=(totSec-day*60*60*24)/(60*60);
-		int min=(totSec-day*60*60*24-hour*3600)/(60);
-		int sec=totSec%60;
+		int day=cho/(24*60*60);//24*60*60 = 86400
+		cho%=86400;//cho=cho%86400;
+		int hour=cho/(60*60);//60*60 = 3600
+		cho%=3600;
+		int min=cho/60;
+		int sec=cho%60;
 		
-		System.out.print("잔여시간은 ");
-		System.out.println(day+"일 "+hour+"시간 "+min+"분 "+sec+"초 입니다");
-
+		System.out.println(day+"일 "+hour+"시 "+min+"분 "+sec+"초");
 		System.out.println("===============================================");
 		//한대의 가격이 1억 5천원만인 비행기를 20대 구매할 경우 지불해야될 금액을 계산하여 출력하세요. 
 		//단, 15대 이상 구매할 경우 1대당 25%의 할인율을 적용하여 계산하세요.
-		//삼항연산자 ,Long 변수 사용
-		int airplane=150_000_000;
-		int buyer=20;
-		double salePercent=0.75;
-		long sale=(long)(airplane*salePercent)*buyer;
-		long nonSale=(long)(airplane*buyer);
-		
-		System.out.println("할인가능여부	"+(buyer>=15?"O":"X"));
-		System.out.println("구매희망수량 = "+buyer);
-		System.out.println("최종가격 = " +(buyer>=15?(long)(sale):(long)(nonSale)));
-		
-		
-		
-		
-		
+		int plane=150_000_000, cnt=20;
+		long money=(long)(cnt>=15?plane*0.75:plane)*cnt;
+		System.out.println("지불 금액 = "+money);
 		System.out.println("===============================================");
 	}
 }
