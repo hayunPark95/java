355a49c7e41b76b03be8f44620a9161447d5beaa
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-04 18:26:46 +0900

    집에가서 마저하기

--- java_1/src/example/UpAndDownExample.java
+++ java_1/src/example/UpAndDownExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Scanner;
+
 //컴퓨터로부터 제공받은 정수 난수값을 키보드로 입력하여 맞추는 프로그램을 작성하세요.
 // => 1~100 범위의 정수 난수값을 제공받도록 작성
 // => 난수값을 맞출 수 있는 기회는 10번만 제공되도록 작성
@@ -9,6 +11,23 @@ package example;
 // => 난수값을 10번 안에 맞추지 못한 경우 난수값이 출력되도록 작성
 public class UpAndDownExample {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("1~100 사이의 정수값을 맞춰보세요" );
+		
+		int num=0;
+		int i=0;
+		num = scanner.nextInt();
+		while(num>100 && num<0) {
+			System.out.println("[에러] 1~100범위의 정수값만 입력하시오");
+			if(num>0 || num<100) break;{
+				i++;
+				System.out.println(i+"번째 시도입니다"+(10-i)+"번의 기회가 남았습니다");
+			}
+			
+		}// 여기까지 1~100 사이의 
+		System.out.println();
+		
+		
 	}
 }
