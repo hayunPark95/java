a1422c9fa02124d10d0d0d16e66e484951eddd31
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-04 12:29:14 +0900

    4/4 6일차 수업 내용

--- java_1/src/basic/Practice1.java
+++ java_1/src/basic/Practice1.java
@@ -1,5 +1,7 @@
 package basic;
 
+import java.util.Scanner;
+
 public class Practice1 {
 
 	public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Practice1 {
 		int x=1;
 		for(int i=1;1<=x;i++) {
 			System.out.print("d");
+			Scanner sc=new Scanner(System.in);
 			
 		}
 	}
