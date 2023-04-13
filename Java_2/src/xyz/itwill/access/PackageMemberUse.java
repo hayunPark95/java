ed3c13a8d5e42da76a4f1c88ea288621116326a3
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-13 16:53:25 +0900

    4/13 13일차

---
+++ Java_2/src/xyz/itwill/access/PackageMemberUse.java
@@ -0,0 +1,12 @@
+package xyz.itwill.access;
+
+public class PackageMemberUse {
+	public void run() {
+		PackageMember member = new PackageMember();
+		
+		member.num = 100;
+		member.display();
+		
+		
+	}
+}
---
+++ Java_2/src/xyz/itwill/other/PackageMemberOtherUse.java
@@ -0,0 +1,10 @@
+package xyz.itwill.other;
+
+public class PackageMemberOtherUse {
+	public void run() {
+//		PackageMember member = new PackageMember();
+		
+//		member.num = 100;
+//		member.display();
+	}
+}
