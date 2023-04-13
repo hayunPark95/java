ade205f4e549f7e986e471253ed3aa1b0fdbc81b
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-13 12:36:40 +0900

    4/13 13일차

--- Java_2/src/nested/OuterOne.java
+++ Java_2/src/nested/OuterOne.java
@@ -1,6 +1,6 @@
 package nested;
 
-//중첩 클래스(Nested Class) : 클래스(OuterClass) 내부에 클래스(InnerClass)를 선언
+//중첩 클래스(Nested Class) : 클래스(OuterClass) 내부에 선언된 클래스(InnerClass) 
 // => 두개의 클래스가 밀접한 관계에 있을 경우 사용 - 클래스의 캡슐화를 강화할 목적으로 선언
 
 public class OuterOne {
@@ -32,12 +32,12 @@ public class OuterOne {
 		
 		//외부클래스는 객체 내부클래스로 객체를 생성하여 접근제한자에 상관없이 객체 내부클래스의
 		//필드 또는 메소드 참조 가능
-		//InnerOne innerOne=new InnerOne();
+		//InnerOne innerOne=new InnerOne(200);
 		//System.out.println("innerNum = "+innerOne.innerNum);
 		//innerOne.innerDisplay();
 	}
 	
-	//객체 내부클래스 - 컴파일 결과를 [외부클래스$내부클래스.class] 파일로 제공
+	//객체 내부클래스(Instance InnerClass) - 컴파일 결과를 [외부클래스$내부클래스.class] 파일로 제공
 	// => 객체 내부클래스에서는 static 제한자를 사용하여 필드 또는 메소드 선언 불가능
 	public class InnerOne {
 		private int innerNum;
