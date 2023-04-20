1b1f3ab2464c26b58419710c2326a33e9e795cb2
ITWILL <ITWILL@DESKTOP-0ICTL43>
2023-04-20 10:14:27 +0900

    4/20 17days 집

--- java-main/Java_3/src/homework/DayCalculateApp.java
+++ java-main/Java_3/src/homework/DayCalculateApp.java
@@ -1,18 +1,11 @@
 package homework;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class DayCalculateApp {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		Date now = new Date();
-		long currentTime = System.currentTimeMillis();
-		String wantDate = scanner.nextLine();
-		scanner.close();
-	
-		
+
 	
 		
 	}
