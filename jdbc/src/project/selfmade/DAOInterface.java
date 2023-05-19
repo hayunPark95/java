d51cf43f650ae877267b9111c01ee254c50d765f
user <user@BOOK-LCCUUB9T5H>
2023-05-19 12:11:18 +0900

    wlrma

--- jdbc/src/project/selfmade/DAOInterface.java
+++ jdbc/src/project/selfmade/DAOInterface.java
@@ -1,5 +1,13 @@
 package project.selfmade;
 
-public interface DAOInterface {
+import java.util.List;
 
+public interface DAOInterface {
+	String insertProject(DTO project);
+	String updateProject(DTO project);
+	String deleteProject(String name);
+	DTO selectProject(String name);
+	
+	List<DTO> selectGenderList(String gender);
+	List<DTO> selectAllList();
 }
--- jdbc/src/project/selfmade/DTO.java
+++ jdbc/src/project/selfmade/DTO.java
@@ -6,4 +6,58 @@ public class DTO {
 	private String mem;
 	private String song;
 	private String agency;
+	
+	public DTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DTO(String name, String gender, String mem, String song, String agency) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.mem = mem;
+		this.song = song;
+		this.agency = agency;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMem() {
+		return mem;
+	}
+
+	public void setMem(String mem) {
+		this.mem = mem;
+	}
+
+	public String getSong() {
+		return song;
+	}
+
+	public void setSong(String song) {
+		this.song = song;
+	}
+
+	public String getAgency() {
+		return agency;
+	}
+
+	public void setAgency(String agency) {
+		this.agency = agency;
+	}
+	
 }
